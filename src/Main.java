4333ea48fe7d5c7bc62095d6e4eae2b35ca20f23
Catzila <Bilet082@mail.ru>
2024-01-13 02:30:06 +0400

    lesson_1

--- src/Main.java
+++ src/Main.java
@@ -13,37 +13,37 @@ public class Main {
     }
 
     public static void checkSumSign() {
-        int a = -1;
-        int b = -1;
+        int a = -21;
+        int b = 1;
         int sum = a + b;
         if (sum >= 0) {
             System.out.println("Сумма положительная");
         }
-        if (sum < 0) {
+        else  {
             System.out.println("Сумма отрицательная");
         }
     }
 
     public static void printColor() {
-        int value = 100;
+        int value = 0;
         if (value <= 0) {
             System.out.println("Красный");
         }
-        if (value > 0 && value <= 100) {
+        else if (value > 0 && value <= 100) {
             System.out.println("Жёлтый");
         }
-        if (value > 100) {
+        else if (value > 100) {
             System.out.println("Зелёный");
         }
     }
 
     public static void compareNumbers() {
-        int a = 5;
-        int b = 5;
+        int a = 513;
+        int b = 51;
         if(a >= b) {
             System.out.println("a >= b");
         }
-        if(a < b) {
+        else  {
             System.out.println("a < b");
         }
     }
