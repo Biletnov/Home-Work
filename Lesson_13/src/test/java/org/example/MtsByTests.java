b2c39ec8ac9cdbb75eff2a07cc92b67e3822f3fd
Catzila <Bilet082@mail.ru>
2024-02-26 14:40:10 +0400

    Lesson_13

---
+++ Lesson_13/src/test/java/org/example/MtsByTests.java
@@ -0,0 +1,75 @@
+package org.example;
+
+//Необходимо написать автотесты для сайта mts.by.
+// Суть тестов заключается в проверке блока «Онлайн пополнение без комиссии»:
+//Проверить название указанного блока;
+// Проверить наличие логотипов платёжных систем;
+// Проверить работу ссылки «Подробнее о сервисе»;
+// Заполнить поля и проверить работу кнопки «Продолжить»
+// (проверяем только вариант «Услуги связи», номер для теста 297777777)
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MtsByTests {
+    
+    private WebDriver driver;
+    private String testPhoneNumber = "297777777";
+    
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\bilet\\IdeaProjects\\Home_work\\Lesson_13\\src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(13, TimeUnit.SECONDS);
+        driver.get("https://mts.by");
+        WebElement cookie = driver.findElement(By.id("cookie-agree"));// Принимает cookie
+        cookie.click();
+    }
+    
+    @Test//Проверка названия блока
+    public void BlockCheckName() {
+        WebElement blockTitle = driver.findElement(By.xpath("//h2[text()='Онлайн пополнение ']"));
+        assertTrue(blockTitle.isDisplayed(), "Онлайн пополнение без комиссии");
+    }
+    
+    @Test//Проверка наличия логотипов платёжных систем
+    public void PayPartnersLogoCheck() {
+        WebElement logoBlock = driver.findElement(By.className("pay__partners"));
+        assertTrue(logoBlock.findElements(By.tagName("img")).size() > 0, "Логотипы не найдены");
+    }
+    
+    @Test//Проверка работы ссылки «Подробнее о сервисе»
+    public void MoreInfoLinkCheck() {
+        WebElement moreInfoLink = driver.findElement(By.xpath("//a[text()='Подробнее о сервисе']"));
+        moreInfoLink.click();
+        assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/", driver.getCurrentUrl());
+    }
+    
+    @Test//Проверка ввода номера и кнопки "Продолжить"
+    public void ButtonCheck() {
+        WebElement serviceTypeDropdown = driver.findElement(By.className("select__now"));
+        assertTrue(serviceTypeDropdown.isDisplayed(), "Услуги связи");
+        WebElement phoneNumberInput = driver.findElement(By.id("connection-phone"));
+        phoneNumberInput.click();
+        phoneNumberInput.sendKeys(testPhoneNumber);
+        WebElement button = driver.findElement(By.xpath("//*[@id='pay-connection']/button"));
+        button.click();
+    }
+    
+    @AfterEach
+    public void kill() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
